4af58c5c712294dfc638cb61671599c8b527ca2e
Paz Bunis <pazbunis@gmail.com>
2016-08-01 17:41:21 +0300

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+#mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
