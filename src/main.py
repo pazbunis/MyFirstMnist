8354cb1a9cb0172498d1f6402e251ba425578869
Paz Bunis <pazbunis@gmail.com>
2016-08-04 10:42:48 +0300

    getting started with MNIST (0.92% accuracy)

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
@@ -11,7 +12,9 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 # correct labels:
 y_ = tf.placeholder(tf.float32, [None, 10])
 
+# loss functions:
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
 
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
