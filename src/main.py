7e26b2276a620f5129592af4e4464affb9947595
Paz Bunis <pazbunis@gmail.com>
2016-08-04 10:16:03 +0300

    continuing MNIST

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
-#mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-print("gef")
+# correct labels:
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
