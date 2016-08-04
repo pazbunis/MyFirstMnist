a9e1de0d53b393ca7aafd7f8c6f6fbd53db43cda
Paz Bunis <pazbunis@gmail.com>
2016-08-04 10:39:50 +0300

    getting started with MNIST (0.92% accuracy)

--- src/main.py
+++ src/main.py
@@ -16,6 +16,15 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
-
+tf.initialize_variables
 sess = tf.Session()
 sess.run(init)
+
+# actual training
+for i in range(10000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
